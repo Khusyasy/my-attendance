546b6809a145aab0f6a800fa7846d2a5c46ae82f
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2023-10-04 11:09:16 +0700

    fix: prisma schema username
    user username should be unique

--- prisma/migrations/20231004040825_init/migration.sql
+++ prisma/migrations/20231004040825_init/migration.sql
@@ -4,8 +4,8 @@ CREATE TYPE "UserRole" AS ENUM ('STUDENT', 'TEACHER', 'ADMIN');
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
     "username" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "role" "UserRole" NOT NULL DEFAULT 'STUDENT',
     "courseId" INTEGER,
@@ -53,6 +53,9 @@ CREATE TABLE "QRCode" (
 
     CONSTRAINT "QRCode_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Course_code_key" ON "Course"("code");
