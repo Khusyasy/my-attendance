336f5417a7fdc05389df126f96eb67c0eee07960
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2023-10-07 11:42:44 +0700

    feat: add vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+    modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+    // https://github.com/vuetifyjs/vuetify/issues/16121#issuecomment-1320867902
+    ssr: {
+      noExternal: ['vuetify'],
+    },
+  },
 })
