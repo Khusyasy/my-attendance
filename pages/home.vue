c97c86f4642943771355c6da28b42792274053e3
Muhammad Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2024-08-22 14:14:23 +0700

    chore: setup prettier, eslint, and tailwindcss

---
+++ pages/home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const count = ref(0);
+</script>
+
+<template>
+  <div class="">
+    <h1>Home page</h1>
+    <button
+      class="rounded bg-green-300 px-2 py-1 hover:bg-green-400"
+      @click="count++"
+    >
+      counter {{ count }}
+    </button>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Index page</h1>
+    <NuxtLink to="home"> Go to home </NuxtLink>
+  </div>
+</template>
