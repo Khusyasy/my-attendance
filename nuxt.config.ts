280c0cb952aa2785c9c58e53c81734cc73c1f2f3
Muhammad Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2024-08-22 13:31:32 +0700

    chore: start project

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  compatibilityDate: '2024-08-22',
+  typescript: {
+    typeCheck: true,
+  },
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
+});
